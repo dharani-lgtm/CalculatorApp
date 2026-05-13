8e8a01a9ab42527499cc4d16ab3b9fea24c79df7
dharani <dharani@qti.qualcomm.com>
2026-05-13 10:49:47 +0530

    added square function to calculator.py file

--- calculator.py
+++ calculator.py
@@ -12,6 +12,10 @@ def multiply(a, b):
 def divide(a, b):
     return a / b
 
+# New function added in feature branch
+def square(a):
+    return a * a
+
 if __name__ == "__main__":
     print("2 + 3 =", add(2, 3))
     print("5 - 2 =", subtract(5, 2))
