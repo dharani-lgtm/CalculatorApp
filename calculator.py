75a4e546a67099f0fa0e71b4c13dfc188867f3f5
dharani <dharani@qti.qualcomm.com>
2026-05-13 10:44:47 +0530

    Intial commit added Calculator.py file to git

---
+++ calculator.py
@@ -0,0 +1,19 @@
+# Calculator App
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+if __name__ == "__main__":
+    print("2 + 3 =", add(2, 3))
+    print("5 - 2 =", subtract(5, 2))
+    print("2 * 3 =", multiply(2, 3))
+    print("6 / 2 =", divide(6, 2))
